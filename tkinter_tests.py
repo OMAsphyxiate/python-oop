207f589b4cee2098dadadc3a430b8a16097d612f
OMAsphyxiate <omasphyxiate@gmail.com>
2017-06-07 17:03:42 -0500

    Changes to tkinter files and adding aging script

--- tkinter_tests.py
+++ tkinter_tests.py
@@ -12,14 +12,15 @@ def open_so(event):
 
 window_frame = tk.Tk() #Create new window
 window_frame.geometry("800x600") #Set dimensions of windows to 800x600
+window_frame.configure(backgroun="gray")
 
-bookmark1 = tk.Button(window_frame, text="Facebook")
+bookmark1 = tk.Button(window_frame, text="Facebook", background="gray")
 bookmark1.grid(column=0, row=0)
 
-bookmark2 = tk.Button(window_frame, text="GMail")
+bookmark2 = tk.Button(window_frame, text="GMail", background="gray")
 bookmark2.grid(column=1, row=0)
 
-bookmark3 = tk.Button(window_frame, text="Stack Overflow")
+bookmark3 = tk.Button(window_frame, text="Stack Overflow", background="gray")
 bookmark3.grid(column=2, row=0)
 
 bookmark1.bind("<Button-1>", open_fb)
