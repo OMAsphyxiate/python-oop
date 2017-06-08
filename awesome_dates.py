5776e661422f820b1dbb7e36339fd2770bf93e7a
OMAsphyxiate <omasphyxiate@gmail.com>
2017-06-08 10:09:06 -0500

    Changed age setup to ask for user input

--- awesome_dates.py
+++ awesome_dates.py
@@ -1,30 +1,19 @@
 import datetime #Imports the datetime Library
 
-setDate = datetime.date(2015, 7, 14) #Creates object from the class datetime.date()
-
-print(setDate)
-print(datetime.date.today()) #Shows current date
-print(datetime.date.today().year) #Shows current year
-print(datetime.date.today().month) #Shows current month
-print(datetime.date.today().day) #Shows current day
-
-
 class Age: #Define Age class
 
 	def __init__(self, year, month, day):
 		self.birthday = datetime.date(year, month, day)
-		self.days = datetime.date.today() - datetime.timedelta(days=birthdate)
+		self.today = datetime.date.today()
+		self.days = self.today - self.birthday
 
 	def __str__(self):
 		return"You are {} days old".format(self.days)
 
+birthyear = input("Please enter your birth year: ")
+birthmonth = input("Please enter your birth month: ")
+birthday = input("Please enter your birth date: ")
 
-birthday1 = Age(2010, 1, 30)
-birthday2 = Age(1986, 10, 6)
-birthday3 = Age(2000, 5, 15)
+UserInput = Age(int(birthyear), int(birthmonth), int(birthday))
 
-
-
-print(birthday1)
-print(birthday2)
-print(birthday3)
+print(UserInput)
